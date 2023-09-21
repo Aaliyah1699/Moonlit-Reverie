7d63968b1a430934d3e7e5a97cf268ca9a4c7a9e
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-09-20 21:34:31 -0500

    Add SubmitBtn Component

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -1,5 +1,28 @@
-const SubmitBtn = () => {
-    return <div>SubmitBtn</div>;
+/* eslint-disable react/prop-types */
+import { useNavigation } from 'react-router-dom';
+
+const SubmitBtn = ({ text }) => {
+    const navigation = useNavigation();
+    const isSubmitting = navigation.state === 'submitting';
+
+    return (
+        <button
+            type='submit'
+            className='btn btn-primary btn-block'
+            disabled={isSubmitting}
+        >
+            {/* Display a loading spinner and "sending" text when submitting */}
+            {isSubmitting ? (
+                <>
+                    <span className='loading loading-spinner'></span>
+                    sending...
+                </>
+            ) : (
+                // Display the provided text or default to "submit"
+                text || 'submit'
+            )}
+        </button>
+    );
 };
 
 export default SubmitBtn;
