997fd3550ab95e20b51641b02eef31487bd499c2
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-09-20 20:05:07 -0500

    setup imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,33 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import {
+    About,
+    Cart,
+    Checkout,
+    Error,
+    HomeLayout,
+    Landing,
+    Login,
+    Orders,
+    Products,
+    Register,
+    SingleProduct,
+} from './pages';
+
+import { ErrorElement } from './components';
+
+// loaders
+import { loader as landingLoader } from './pages/Landing';
+import { loader as singleProductLoader } from './pages/SingleProduct';
+import { loader as productsLoader } from './pages/Products';
+import { loader as checkoutLoader } from './pages/Checkout';
+import { loader as ordersLoader } from './pages/Orders';
+// actions
+import { action as registerAction } from './pages/Register';
+import { action as loginAction } from './pages/Login';
+import { action as checkoutAction } from './components/CheckoutForm';
+import { store } from './store';
 
 const App = () => {
     return <div>App</div>;
